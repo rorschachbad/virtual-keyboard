aeae3319ada7cd997c18a391921150967b952ae6
rorschachbad <72745338+rorschachbad@users.noreply.github.com>
2023-05-01 19:33:27 +0300

    feat: added function shift key

--- src/index.js
+++ src/index.js
@@ -3,25 +3,39 @@ const inputKeyboard = document.querySelector('textarea');
 const keys = document.querySelectorAll('.key');
 const container = document.querySelector('.container');
 
+const arrKeys = Array.from(keys);
 
+const defaultKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 
+'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 
+'\\', 'del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 
+'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 
+'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
 
- const pressShiftKeyboard = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 
- 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'del', 
- 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 
- 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '', 'Shift', 
- 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '', '', '', 'Ctrl'];
+const pressShiftKeyboard = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 
+'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'del', 
+'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 
+'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift', 
+'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
 
-
+const rusKeyboard = ['ё', '1', '2', '3', '4', '5', '6', '7',
+ '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у',
+ 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'del', 
+ 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 
+ "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 
+ 'ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', 
+ '↓', '→', 'Ctrl'];
 
 let isCapslockActive = false;
-
-
+let pressShiftActive = false;
 
 function isKeyDown(e) {
-    console.log(e);
     Array.from(keys).forEach(key => {
-        if (key.classList.contains(e.keyCode)) {
-            key.classList.toggle('active');
+        if (key.classList.contains(e.keyCode) && !key.classList.contains('active')  
+        && !key.classList.contains('rshift') && !key.classList.contains('rctrl')  
+        && !key.classList.contains('ralt')) {
+            key.classList.add('active');
+        } else if (key.classList.contains('active') && !key.classList.contains('rshift')) {
+            key.classList.remove('active');
         }
     })
 }
@@ -40,7 +54,8 @@ function inputTeaxtarea(e) {
     Array.from(keys).forEach(key => {
         if (key.textContent === e.key && key.textContent !== 'Backspace' 
         && key.textContent !== 'Tab' && key.textContent !== 'CapsLock'
-        && key.textContent !== 'Enter') {
+        && key.textContent !== 'Enter' && key.textContent !== 'Shift'
+        && key.textContent !== 'Alt') {
                 inputKeyboard.value += event.key;
         } 
     })
@@ -59,6 +74,12 @@ function Tab(e) {
     }
 }
 
+function alt(e) {
+    if (e.keyCode === 18) {
+        e.preventDefault();
+    }
+}
+
 function CapsLock(e) {
     if (e.keyCode === 20) {
         isCapslockActive = !isCapslockActive;
@@ -72,16 +93,13 @@ function CapsLock(e) {
         
         const strUniqLetters = Array.from(setUniqLetters).join();
 
-        console.log(strUniqLetters);
 
         Array.from(keys).forEach(key => {
             for (let i = 0; i < strUniqLetters.length; i++) {
                 if (key.textContent.toLowerCase() === strUniqLetters[i].toLowerCase() && isCapslockActive) {
                     key.textContent = key.textContent.toUpperCase();
-                    console.log('Uppercase');
                 } else if (key.textContent.toLowerCase() === strUniqLetters[i].toLowerCase() && !isCapslockActive) {
                     key.textContent = key.textContent.toLowerCase();
-                    console.log('lowercase');
                 }             
             }
         })
@@ -89,32 +107,33 @@ function CapsLock(e) {
     }
 }
 
-const rusKeyboard = ['ё', '1', '2', '3', '4', '5', '6', '7',
- '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у',
- 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'del', 
- 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 
- "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 
- 'ю', '.', '', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '', 
- '', '', 'Ctrl'];
 
-let changeLangActive = false;
 
-const KEYS = Array.from(keys);
-
-function changeLanguage(e) {
+function pressShiftDown(e) {
     if (e.keyCode === 16) {
-        changeLangActive = !changeLangActive;
-        for (let i = 0; i < KEYS.length; i++) {
-            for (let k = 0; k < rusKeyboard.length; k++) {
-                if (KEYS[i] !== rusKeyboard[k] && changeLangActive) {
-                    KEYS[i].textContent = rusKeyboard[i];
-                }
+        pressShiftActive = !pressShiftActive;
+        for (let i = 0; i < arrKeys.length; i++) {
+            for (let i = 0; i < pressShiftKeyboard.length; i++) {
+                if (e.key !== pressShiftKeyboard[i] && pressShiftActive) {
+                    arrKeys[i].textContent = pressShiftKeyboard[i];
+                }   
             }
         }
     }
 }
 
-document.addEventListener('keydown', changeLanguage)
+function pressShiftUp(e) {
+    if (e.keyCode === 16) {
+        pressShiftActive = !pressShiftActive;
+        for (let i = 0; i < arrKeys.length; i++) {
+            for (let i = 0; i < defaultKeyboard.length; i++) {
+                if (e.key !== defaultKeyboard[i] && !pressShiftActive) {
+                    arrKeys[i].textContent = defaultKeyboard[i];
+                }   
+            }
+        }
+    }
+}
 
 function Space(e) {
     if (e.keyCode === 32) {
@@ -148,29 +167,27 @@ function ArrowRight(e) {
         inputKeyboard.value += '→';
     }
 }
+
+document.addEventListener('keydown', alt)
 document.addEventListener('keydown', ArrowLeft);
 document.addEventListener('keydown', ArrowTop);
 document.addEventListener('keydown', ArrowBottom);
 document.addEventListener('keydown', ArrowRight);
-
 document.addEventListener('keydown', Enter);
 document.addEventListener('keydown', Enter);
 document.addEventListener('keydown', Enter);
-
-
 document.addEventListener('keydown', Enter);
 document.addEventListener('keydown', Space);
-
 document.addEventListener('keydown', CapsLock);
-
 document.addEventListener('keydown', Tab);
-
-
 document.addEventListener('keydown', isKeyDown);
-document.addEventListener('keyup', isKeyUp);
-
 document.addEventListener('keydown', inputTeaxtarea);
 document.addEventListener('keydown', BackSpace);
+document.addEventListener('keydown', pressShiftDown)
+
+document.addEventListener('keyup', pressShiftUp)
+document.addEventListener('keyup', isKeyUp);
+
 
 
     
